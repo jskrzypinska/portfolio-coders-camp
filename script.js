c501254361069f854eae526d5a6409d60af604cd
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-11-02 16:47:16 +0100

    uploaded project

---
+++ script.js
@@ -0,0 +1,8 @@
+let burgerButton = document.querySelector("#burger");
+let menuList = document.querySelector("ul");
+
+burgerButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menuList.classList.toggle("menu-fold");
+}
